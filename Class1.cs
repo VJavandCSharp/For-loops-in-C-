5bcac912a35c052c902dc6c9a90190743b6ac8a3
VJavandCSharp <82200050+VJavandCSharp@users.noreply.github.com>
2021-04-09 14:17:29 +0800

    Create Class1.cs
    The class is useful for demonstrating basic for-loops in C# (C-Sharp)

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i); // Prints 0, 1, 2, 3, 4 <-- 5th index
+        }
+    }
+}
